1d9d8e34065272e2d0ad5bddad2a778aa1ac2732
kt815 <kt.815@mail.ru>
2016-05-16 03:30:24 +0800

    Added admin theme for login in sites/all/themes/kt/template.php

--- sites/all/themes/kt/template.php
+++ sites/all/themes/kt/template.php
@@ -4,5 +4,10 @@ function kt_preprocess_page(&$vars, $hook) {
     unset($vars['page']['content']['system_main']['default_message']);
 }
 
+function kt_admin_paths_alter(&$paths) {
+    // Treat all user pages as not administrative.
+    $paths['user'] = TRUE;
+}
+
 
 ?>
